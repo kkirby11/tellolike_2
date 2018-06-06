f4b83fce29a563fe64a36f81f05d387c4f9f3587
kristina kirby <kristinakirby@mac.com>
2018-06-06 09:32:20 -0600

    7 commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_many :member_accounts, dependent: :destroy
-  has_many :boards, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  validates_presence_of :first_name, :last_name, 
+  has_many :member_accounts, dependent: :destroy
+  has_many :boards, dependent: :destroy
+  validates_presence_of :first_name, :last_name 
 end
